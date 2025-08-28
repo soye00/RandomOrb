306de99fac32a57a17edc4e9b15d52a3f11fce28
소연 <ssssssy0@naver.com>
2025-08-28 15:10:18 +0900

    feat(pwa): add manifest and service worker

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { registerSW } from 'virtual:pwa-register'
+
+registerSW()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
